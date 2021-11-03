1456732e871ae235c0e2aeefcab81436b1820540
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2021-11-03 16:26:21 -0500

    Ai start

---
+++ Break Room Unity/Assets/Scripts/JacksonDevelopedScripts/AIScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIScript : MonoBehaviour
+{
+    public float lookRadius;
+    private float minDistance = 8f;
+
+    public Transform target;
+    NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            FaceTarget();
+
+            if (distance <= agent.stoppingDistance)
+            {
+                //FaceTarget();
+                
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    /*IEnumerator hitTime()
+    {
+        
+    }*/
+}
