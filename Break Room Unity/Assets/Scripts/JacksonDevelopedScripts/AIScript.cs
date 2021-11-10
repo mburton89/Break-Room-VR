424f047003a6bf890536d6997dad36c20c159ba1
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2021-11-10 16:22:31 -0600

    AI random movement and enemy launcher implemented

--- Break Room Unity/Assets/Scripts/JacksonDevelopedScripts/AIScript.cs
+++ Break Room Unity/Assets/Scripts/JacksonDevelopedScripts/AIScript.cs
@@ -6,17 +6,22 @@ using UnityEngine.AI;
 public class AIScript : MonoBehaviour
 {
     public float lookRadius;
-    private float minDistance = 8f;
+    private float minDistance = 5f;
+    private float maxDistance = 15f;
+    private Vector3 targetSpot;
+    private bool setPos = true;
+
+    public EnemyLauncher launcher;
 
     public Transform target;
     NavMeshAgent agent;
-
 
     // Start is called before the first frame update
     void Start()
     {
         //target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        Debug.Log(transform.forward);
     }
 
     // Update is called once per frame
@@ -24,7 +29,25 @@ public class AIScript : MonoBehaviour
     {
         float distance = Vector3.Distance(target.position, transform.position);
 
-        if (distance <= lookRadius)
+        
+        if (setPos == true || targetSpot == new Vector3(0f,0f,0f))
+        {
+            launcher.LaunchGrenade();
+            targetSpot = RandomNavSphere(target.position, maxDistance, minDistance, -1);
+            targetSpot = (target.position - targetSpot).normalized * Random.Range(minDistance, maxDistance);
+            Debug.Log(distance);
+            setPos = false;
+            StartCoroutine(moveTime());
+        }
+        else
+        {
+            agent.SetDestination(targetSpot);
+            FaceTarget();
+        }
+
+
+
+        /*if (distance <= lookRadius)
         {
             agent.SetDestination(target.position);
             FaceTarget();
@@ -34,7 +57,7 @@ public class AIScript : MonoBehaviour
                 //FaceTarget();
                 
             }
-        }
+        }*/
     }
 
     void FaceTarget()
@@ -50,8 +73,23 @@ public class AIScript : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 
-    /*IEnumerator hitTime()
+    public static Vector3 RandomNavSphere(Vector3 origin, float distance, float minDistance, int layermask)
     {
-        
-    }*/
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * distance;
+
+        randomDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
+
+        return navHit.position;
+    }
+
+
+    IEnumerator moveTime()
+    {
+        yield return new WaitForSeconds(2.5f);
+        setPos = true;
+    }
 }
--- Break Room Unity/Assets/Scripts/JacksonDevelopedScripts/EnemyLauncher.cs
+++ Break Room Unity/Assets/Scripts/JacksonDevelopedScripts/EnemyLauncher.cs
@@ -2,27 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Launcher : MonoBehaviour
+public class EnemyLauncher : MonoBehaviour
 {
     public Transform spawnPoint;
     public GameObject grenadePrefab;
     private float range = 20f;
-    public KeyCode launchKey;
 
     void Start()
     {
-
+        Debug.Log(transform.forward);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(launchKey))
-        {
-            LaunchGrenade();
-        }
+        
     }
 
-    void LaunchGrenade()
+    public void LaunchGrenade()
     {
         GameObject newGrenade = Instantiate(grenadePrefab, spawnPoint.position, spawnPoint.rotation);
         newGrenade.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * range, ForceMode.Impulse);
